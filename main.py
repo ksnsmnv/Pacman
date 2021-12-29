3ca704e315112ddcd33975c51f02736aa22c1aa0
ksnsmnv <ks.semenova@icloud.com>
2021-12-29 17:17:59 +0300

    add class Pacman and PacmanMoves

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import pygame
 
 
+TILE_SIZE = 18
+START_POSITION = (1, 1)
+FREE_TILES = [1]
+
+
 def main():
     pygame.init()
     size = 800, 600
     screen = pygame.display.set_mode(size)
-    # создание экземпляра лабиринта (из текствого файла в матрицу)
+    # создание экзепляра лабиринта (из текствого файла в матрицу)
     labyrinth = Labyrinth()
+    # создание экземпляра пакмана
+    pacman = Pacman(START_POSITION)
+    # создание экземпляра PacmanMoves, который задает движение пакмана
+    pacman_moves = PacmanMoves(labyrinth, pacman)
     clock = pygame.time.Clock()
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        # создание самого лабиринта (из матрицы в виджет pygame)
+        pacman_moves.change_pos()
         screen.fill((0, 0, 0))
+        # создание самого лабиринта (из матрицы в виджет pygame)
         labyrinth.make(screen)
+        pacman.make(screen)
         pygame.display.flip()
         clock.tick(15)
     pygame.quit()
@@ -26,24 +37,67 @@ class Labyrinth:
         self.map = []
         with open("pacman_light_labyrinth.txt") as text_lab:
             for line in text_lab:
-                # создание матрицы со значениями "1", "0", отвечающими за стены и пустые части либиринта
+                # создание матрицы либиринта
                 self.map.append(list(map(int, line.split())))
         self.height = len(self.map)
         self.width = len(self.map[0])
-        self.tile_size = 17
-        self.free_tiles = [1]
 
     def make(self, screen):
         # цвета для  каждого символа в лабиринте
-        # 0 - стена, 1 - можно ходить(по белому), 9 - место за полем, 2 - могут ходить только приведения
-        colors = {0: (0, 0, 120), 1: (255, 255, 255), 9: (0, 0, 0), 2: (0, 100, 0)}
+        # 0 - стена,
+        # 1 - можно ходить(по белому),
+        # 9 - место за полем,
+        # 2 - могут ходить только приведения
+        colors = {0: (0, 0, 120), 1: (255, 255, 255),
+                  9: (0, 0, 0), 2: (0, 100, 0)}
         for y in range(self.height):
             for x in range(self.width):
-                rect = pygame.Rect(x * self.tile_size, y * self.tile_size, self.tile_size, self.tile_size)
+                rect = pygame.Rect(x * TILE_SIZE, y * TILE_SIZE,
+                                   TILE_SIZE, TILE_SIZE)
                 screen.fill(colors[self.get_tile_id((x, y))], rect)
 
     def get_tile_id(self, position):
         return self.map[position[1]][position[0]]
+
+    def tile_is_free(self, position):
+        return self.get_tile_id(position) in FREE_TILES
+
+
+class Pacman:
+    def __init__(self, position):
+        self.x, self.y = position
+        print(self.x, self.y)
+
+    def set_position(self, position):
+        self.x, self.y = position
+
+    def get_position(self):
+        return self.x, self.y
+
+    def make(self, screen):
+        # создание пакмана в виде шарика
+        center = self.x * TILE_SIZE + TILE_SIZE // 2, \
+                self.y * TILE_SIZE + TILE_SIZE // 2
+        pygame.draw.circle(screen, (232, 167, 2), center, TILE_SIZE // 2)
+
+
+class PacmanMoves:
+    def __init__(self, labyrinth, pacman):
+        self.labyrinth = labyrinth
+        self.pacman = pacman
+
+    def change_pos(self):
+        new_x, new_y = self.pacman.get_position()
+        if pygame.key.get_pressed()[pygame.K_LEFT]:
+            new_x -= 1
+        if pygame.key.get_pressed()[pygame.K_RIGHT]:
+            new_x += 1
+        if pygame.key.get_pressed()[pygame.K_UP]:
+            new_y -= 1
+        if pygame.key.get_pressed()[pygame.K_DOWN]:
+            new_y += 1
+        if self.labyrinth.tile_is_free((new_x, new_y)):
+            self.pacman.set_position((new_x, new_y))
 
 
 if __name__ == '__main__':
