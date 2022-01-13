c33bf25fdce1741b468fdbb9242a03b228610af2
ksnsmnv <ks.semenova@icloud.com>
2022-01-13 18:20:38 +0300

    core info

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-def my_superfunction():
-    print('What an awesome print!')
+import pygame
+
+
+WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 480, 480
+FPS = 15
 
 
 def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('Again')
-    print('UFO came and added this line')
-    my_superfunction()
+    pygame.init()
+    screen = pygame.display.set_mode(WINDOW_SIZE)
 
-
-if __name__ == '__main__':
-    main()
+    click = pygame.time.Clock()
+    running = True
+    while running:
+        for event in pygame.event.get();
+        if event.tyoe == pygame.QUIT:
+            running = False
+        screen.fill((0, 0, 0))
+        pygame.display.filp()
+        click.tilck(FPS)
