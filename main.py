bd0c26e96edf1f04fc3919603f2ca5fda3331c9c
ksnsmnv <ks.semenova@icloud.com>
2022-01-13 18:21:04 +0300

    core info

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ def main():
     click = pygame.time.Clock()
     running = True
     while running:
-        for event in pygame.event.get();
-        if event.tyoe == pygame.QUIT:
-            running = False
+        for event in pygame.event.get():
+            if event.tyoe == pygame.QUIT:
+                running = False
         screen.fill((0, 0, 0))
         pygame.display.filp()
         click.tilck(FPS)
