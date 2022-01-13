ca75352ae730efd4e7e69dc32628f03d8728e283
ksnsmnv <ks.semenova@icloud.com>
2022-01-13 19:27:34 +0300

    working enemy moves

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 480, 480
 FPS = 15
 MAPS_DIR = "maps"
 TILE_SIZE = 32
+ENEMY_EVENT_TYPE = 30
 
 
 class Labyrinth:
@@ -17,7 +18,7 @@ class Labyrinth:
         self.width = len(self.map[0])
         self.tile_size = TILE_SIZE
         self.free_tiles = free_tiles
-        self.finish_tile - finish_tile
+        self.finish_tile = finish_tile
 
     def render(self, screen):
         colors = {0: (0, 0, 0), 1:(120, 120, 120), 2:(50, 50, 50)}
@@ -30,20 +31,114 @@ class Labyrinth:
     def get_tile_id(self, position):
         return self.map[position[1]][position[0]]
 
+    def is_free(self, position):
+        return self.get_tile_id(position) in  self.free_tiles
+
+    def find_path_step(self, start, target):
+        INF = 1000
+        x, y = start
+        distance = [[INF] * self.width for _ in range(self.height)]
+        distance[y][x] = 0
+        prev = [[None] * self.width for _ in range(self.height)]
+        queue = [(x, y)]
+        while queue:
+            x, y = queue.pop(0)
+            for dx, dy in (1, 0), (0, 1), (-1, 0), (0, -1):
+                next_x, next_y = x + dx, y + dy
+                if 0 <= next_x < self.width and 0 < next_y < self.height and \
+                        self.is_free((next_x, next_y)) and distance[next_y][next_x] == INF:
+                    distance[next_y][next_x] = distance[y][x] + 1
+                    prev[next_y][next_x] = (x, y)
+                    queue.append((next_x, next_y))
+        x, y = target
+        if distance[y][x] == INF or start == target:
+            return start
+        while prev[y][x] != start:
+            x, y = prev[y][x]
+        return x, y
+
+
+class Hero:
+    def __init__(self, position):
+        self.x, self.y = position
+
+    def get_position(self):
+        return self.x, self.y
+
+    def set_position(self, position):
+        self.x, self.y = position
+
+    def render(self, screen):
+        center = self.x * TILE_SIZE + TILE_SIZE // 2, self.y * TILE_SIZE + TILE_SIZE // 2
+        pygame.draw.circle(screen, (255, 255, 255), center, TILE_SIZE // 2)
+
+
+class Enemy:
+    def __init__(self, position):
+        self.x, self.y = position
+        self.delay = 100
+        pygame.time.set_timer(ENEMY_EVENT_TYPE, self.delay)
+
+    def get_position(self):
+        return self.x, self.y
+
+    def set_position(self, position):
+        self.x, self.y = position
+
+    def render(self, screen):
+        center = self.x * TILE_SIZE + TILE_SIZE // 2, self.y * TILE_SIZE + TILE_SIZE // 2
+        pygame.draw.circle(screen, (255, 120, 120), center, TILE_SIZE // 2)
+
+
+class Game:
+    def __init__(self, labyrinth, hero, enemy):
+        self.labyrinth = labyrinth
+        self.hero = hero
+        self.enemy = enemy
+
+    def render(self, screen):
+        self.labyrinth.render(screen)
+        self.hero.render(screen)
+        self.enemy.render(screen)
+
+    def update_hero(self):
+        next_x, next_y = self.hero.get_position()
+        if pygame.key.get_pressed()[pygame.K_LEFT]:
+            next_x -= 1
+        if pygame.key.get_pressed()[pygame.K_RIGHT]:
+            next_x += 1
+        if pygame.key.get_pressed()[pygame.K_UP]:
+            next_y -= 1
+        if pygame.key.get_pressed()[pygame.K_DOWN]:
+            next_y += 1
+        if self.labyrinth.is_free((next_x, next_y)):
+            self.hero.set_position((next_x, next_y))
+
+    def move_enemy(self):
+        next_position = self.labyrinth.find_path_step(self.enemy.get_position(),
+                                                      self.hero.get_position())
+        self.enemy.set_position(next_position)
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode(WINDOW_SIZE)
-    labyrinth = Labyrinth('simlpe_map.txt', [0, 2], 2)
+    labyrinth = Labyrinth('simple_map.txt', [0, 2], 2)
+    hero = Hero((7, 7))
+    enemy = Enemy((7, 1))
+    game = Game(labyrinth, hero, enemy)
     click = pygame.time.Clock()
     running = True
     while running:
         for event in pygame.event.get():
-            if event.tyoe == pygame.QUIT:
+            if event.type == pygame.QUIT:
                 running = False
+            if event.type == ENEMY_EVENT_TYPE:
+                game.move_enemy()
+        game.update_hero()
         screen.fill((0, 0, 0))
-        pygame.display.filp()
-        click.tilck(FPS)
+        game.render(screen)
+        pygame.display.flip()
+        click.tick(FPS)
     pygame.quit()
 
 
